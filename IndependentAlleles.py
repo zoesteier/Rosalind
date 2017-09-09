ec5386465c05154592445e4ef6753dba4dc51d66
Zoë Steier <zoe.steier@gmail.com>
2017-09-08 17:14:07 -0700

    Problem 21: Independent Alleles
    21: independent alleles (combinations, factorial)

--- IndependentAlleles.py
+++ IndependentAlleles.py
@@ -24,3 +24,25 @@ s = input('Enter k and N: ') # k = generation, N = number of AaBb organisms
 splitin = s.split()
 k = int(splitin[0])
 N = int(splitin[1])
+
+import math
+# calculate factorials for combinations
+
+def nChoosek(n, k):
+    '''Calculate combinations n choose k using math factorial function.'''
+    f = math.factorial
+    ans = f(n) // f(k) // f(n-k) # nChoosek = n!/(k!(n-k)!)
+    return(ans)
+    
+# the probability of getting exactly some offspring of AaBb is the binomial coefficient (same as nchoosek) * p(AaBb) *p(not AaBb)
+    
+# the total probability of at least N offspring is 1 - p(everything less than N)
+    
+tot = 1 # starting probability
+# offspring per generation = 2**k
+pAaBb = 0.25
+pnot = 0.75
+for i in range(N):
+    tot -= nChoosek(2**k, i) * pAaBb**i * pnot**(2**k - i)
+    
+print(tot)
